0075be7c72c5473ccd924a0edf5ce9a3b45f04fb
Qveezo <keynpro228@gmail.com>
2023-01-11 20:15:54 +0300

    Added main Vue code

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,80 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+     color="red"
+     dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon v-on:click="navigation = !navigation"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Лучшие новости в мире!
+      </v-toolbar-title>
     </v-app-bar>
+    <v-navigation-drawer v-model="navigation" absolute temporary app>
+      
+    </v-navigation-drawer>
+    <v-content>
+      <v-card>
+              <v-subheader>{{ card }}</v-subheader>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+              <v-list two-line>
+                <template v-for="n in 6">
+                  <v-list-item
+
+                    :key="n"
+                  >
+                    <v-list-item-avatar color="grey darken-1">
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title>Message {{ n }}</v-list-item-title>
+
+                      <v-list-item-subtitle>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="n !== 6"
+                    :key="`divider-${n}`"
+                    inset
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
-  }),
-};
+    navigation: false,
+    news: [],
+    cards: ['Today', 'Yesterday'],
+      drawer: null,
+      links: [
+        ['mdi-inbox-arrow-down', 'Inbox'],
+        ['mdi-send', 'Send'],
+        ['mdi-delete', 'Trash'],
+        ['mdi-alert-octagon', 'Spam'],
+      ],
+    }),
+    methods:{
+      getNews(){
+        this.axios({
+          method: 'GET',
+          url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=d7f41a32c26b4bbfb596d58b1a54c766",
+        }).then((response) => {
+          this.news = response.data.articles;
+        })
+    },
+    mounted(){
+      this.getNews();
+    },
+  };
 </script>
